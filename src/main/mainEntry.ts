dbf3a6414bc960e745857620107ddc05ff1ec3f6
BILIBILI\v_zhangjiaming <https://git.bilibili.co/>
2023-02-10 14:49:25 +0800

    feat: 环境配置

---
+++ src/main/mainEntry.ts
@@ -0,0 +1,27 @@
+// src\main\mainEntry.ts
+import { app, BrowserWindow } from "electron";
+import { CustomScheme } from "./CustomScheme";
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+let mainWindow: BrowserWindow;
+
+app.whenReady().then(() => {
+  let config = {
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      contextIsolation: false,
+      webviewTag: true,
+      spellcheck: false,
+      disableHtmlFullscreenWindowResize: true,
+    },
+  };
+  mainWindow = new BrowserWindow(config);
+  if (process.argv[2]) {
+    mainWindow.webContents.openDevTools({ mode: "undocked" });
+    mainWindow.loadURL(process.argv[2]);
+  } else {
+    CustomScheme.registerScheme();
+    mainWindow.loadURL(`app://index.html`);
+  }
+});
